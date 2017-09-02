9c98e28faf03751749dfd5863c29a36d656dcfdb
tidezyc <tide.zyc@gmail.com>
2017-09-02 20:20:09 +0800

    add support to teamcity 2017

--- src/main/java/jetbrains/buildServer/auth/oauth/AuthenticationSchemeProperties.java
+++ src/main/java/jetbrains/buildServer/auth/oauth/AuthenticationSchemeProperties.java
@@ -1,6 +1,6 @@
 package jetbrains.buildServer.auth.oauth;
 
-import jetbrains.buildServer.serverSide.ServerSettings;
+import jetbrains.buildServer.serverSide.SBuildServer;
 import jetbrains.buildServer.serverSide.auth.AuthModule;
 import jetbrains.buildServer.serverSide.auth.LoginConfiguration;
 import org.jetbrains.annotations.NotNull;
@@ -13,18 +13,18 @@ import java.util.Optional;
 public class AuthenticationSchemeProperties {
 
     private final ConfigPresets presets;
-    private final ServerSettings serverSettings;
+    private final SBuildServer sBuildServer;
     private final LoginConfiguration loginConfiguration;
 
-    public AuthenticationSchemeProperties(@NotNull final ServerSettings serverSettings, @NotNull final LoginConfiguration loginConfiguration) {
-        this.serverSettings = serverSettings;
+    public AuthenticationSchemeProperties(@NotNull final SBuildServer sBuildServer, @NotNull final LoginConfiguration loginConfiguration) {
+        this.sBuildServer = sBuildServer;
         this.loginConfiguration = loginConfiguration;
         this.presets = new ConfigPresets();
     }
 
     @Nullable
     public String getRootUrl() {
-        return serverSettings.getRootUrl();
+        return sBuildServer.getRootUrl();
     }
 
     @Nullable
